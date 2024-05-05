f4900d2936833de7ba411da72de15565cac28336
mattmeyerink <meyerink@umich.edu>
2024-05-04 21:22:18 -0400

    very basic app flow

--- README.md
+++ README.md
@@ -10,6 +10,14 @@ This app is a bullet journal style planner in the CLI that relys on easy to back
 This allows you to be in control of all of your own data at all times while allowing us to 
 save on server costs and prevent us from needing to build out a UI from scratch.
 
+Notes are saved using the following structure.
+All Notes are saved in the /Documents directory
+
+Week Notes
+Filename - `month.mondayDate.year.weekNotes.txt`
+Each line is a note in the format noteID-date-dayOfTheWeek-noteType-isComplete-note-lastUpdated
+
+
 ### MilesOnMiles
 
 Running mileage and and workout tracker that relys on easy to backup text files!
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,73 @@
-use clap::Parser;
-
-#[derive(Parser)]
-struct CLI {
-    app: String,
-    path: std::path::PathBuf,
-}
+use std::io;
 
 fn main() {
-    let args = CLI::parse();
-    let content = std::fs::read_to_string(&args.path).expect("Could not not read file");
+    println!("Welcome to the Wizard Boy Productions CLI tool!");
+    println!("Enjoy the Magic!!!");
+    println!("");
+    println!("Select an app (planit)");
 
-    for line in content.lines() {
-        println!("{}", line);
+    let mut app = String::new();
+
+    io::stdin().read_line(&mut app)
+        .expect("Failed to read app");
+
+    if app.trim() == "planit" {
+        println!("Which view do you want to take? (Week, Month, Year)");
+
+        let mut view = String::new();
+        io::stdin().read_line(&mut view).expect("Unable to read view");
+
+        if view.trim() == "Week" {
+            // Attempt to pull the text file that has this weeks notes
+            
+            // Display the week's notes
+            println!("Week View");
+            println!("Monday");
+            println!("--------------");
+            println!("Tuesday");
+            println!("--------------");
+            println!("Wednesday");
+            println!("--------------");
+            println!("Thursday");
+            println!("--------------");
+            println!("Friday");
+            println!("--------------");
+            println!("Saturday");
+            println!("--------------");
+            println!("Sunday");
+            println!("--------------");
+
+            println!("");
+            println!("");
+
+            println!("What would you like to do?");
+            println!("[1]: Edit a current note.");
+            println!("[2]: Add new note.");
+            println!("[3]: Next week.");
+            println!("[4]: Previous week.");
+
+            let mut action = String::new();
+            io::stdin().read_line(&mut action).expect("Unable to read action");
+
+            if action.trim() == "1" {
+                // Edit a current note.
+            } else if action.trim() == "2" {
+                // Add a new note
+            } else if action.trim() == "3" {
+                // Move forward to the next week
+            } else if action.trim() == "4" {
+                // Move backward to the previous week
+            } else {
+                println!("Boooo thats not a correct action");
+            }
+        } else if view.trim() == "Month" {
+            
+        } else if view.trim() == "Year" {
+
+        } else {
+            println!("That is not a supported view");
+        }
+    } else {
+        println!("That is not a supported app");
     }
 }
