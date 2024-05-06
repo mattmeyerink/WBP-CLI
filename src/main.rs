cfc624d0a3fee61c051f53a8ebb58b0e51ee199a
mattmeyerink <meyerink@umich.edu>
2024-05-05 23:54:49 -0400

    get some stuff actually saving in the file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 use std::fs;
 use dirs::home_dir;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 fn main() {
     println!("Welcome to the Wizard Boy Productions CLI tool!");
@@ -21,6 +23,14 @@ fn main() {
         .expect("Failed to read app");
 
     if app.trim() == "planit" {
+        // Make the planit data folder if it doesn't already exist
+        let wbp_plan_it_data_path = home_dir().unwrap().join("Documents").join("wbp-data").join("plan-it");
+        if !wbp_plan_it_data_path.exists() {
+            fs::create_dir(&wbp_plan_it_data_path).expect("Unable to make plan it dir");
+        }
+
+        // TODO -> Maybe add a step to create a year directory
+
         println!("Which view do you want to take? (Week, Month, Year)");
 
         let mut view = String::new();
@@ -28,10 +38,16 @@ fn main() {
 
         if view.trim() == "Week" {
             // Attempt to pull the text file that has this weeks notes
-            let file_path = "~/Documents/wbp-data/test.txt";
-            let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+            let week_file_path = home_dir().unwrap().join("Documents").join("wbp-data").join("plan-it").join("test.txt");
+            if !week_file_path.exists() {
+                // TODO -> Create the file once I can look up how to do that
+            }
+    
+            let contents = fs::read_to_string(week_file_path).expect("Should have been able to read the file");
 
-            println!("{:?}", contents.lines());
+            for line in contents.lines() {
+                println!("{}", line);
+            }
 
             // Display the week's notes
             println!("Week View");
@@ -65,7 +81,47 @@ fn main() {
             if action.trim() == "1" {
                 // Edit a current note.
             } else if action.trim() == "2" {
+                // Actually just have them put in the day of the week and associate that with 
+                // The right date
+                println!("Enter the day of the week to add it to");
+                let mut day_of_week = String::new();
+                io::stdin().read_line(&mut day_of_week).expect("Unable to read date");
+
+                println!("Enter the type of note (task/event/note)");
+                let mut note_type = String::new();
+                io::stdin().read_line(&mut note_type).expect("Unable to read note type");
+
+                println!("Enter your note");
+                let mut note = String::new();
+                io::stdin().read_line(&mut note).expect("Unable to read note");
+
+                // TODO -> This needs to be a unique id
+                let note_id = "UNIQUE_ID";
+
+                // TODO -> Need to do this so we actully know which date it is
+                let date = "SomeDate";
+
+                // TODO -> This needs to be calculated
+                let modified_date_time = "SomeDateTimeNumbers";
+
+                let is_complete = "false";
+
+                let new_note = format!("{}-{}-{}-{}-{}-{}-{}\n", note_id, date, day_of_week.trim(), note_type.trim(), is_complete, note.trim(), modified_date_time);
+
+                let file_path = home_dir().unwrap().join("Documents").join("wbp-data").join("plan-it").join("test.txt");
+
                 // Add a new note
+                let mut data_file = OpenOptions::new()
+                    .append(true)
+                    .open(file_path)
+                    .expect("cannot open file");
+
+                // Write to a file
+                data_file
+                    .write(new_note.as_bytes())
+                    .expect("write failed");
+
+                println!("Appended content to a file");
             } else if action.trim() == "3" {
                 // Move forward to the next week
             } else if action.trim() == "4" {
