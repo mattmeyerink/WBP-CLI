ebba7870f12cf1f3d5fac037d1b670b8f00a06b9
mattmeyerink <meyerink@umich.edu>
2024-06-23 21:11:17 -0400

    lay the groundwork for month view

---
+++ src/planit/month/mod.rs
@@ -0,0 +1,11 @@
+mod utils;
+mod display;
+mod data;
+mod actions;
+
+pub fn month_view() {
+    let mut current_date = chrono::Local::now();
+
+    // Ask the user if they want to enter list view of if they want to enter highlight view
+    // Have a loop over the whole thing and a loop each for highlight view and list view
+}
