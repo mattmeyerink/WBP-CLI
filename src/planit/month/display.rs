83253d1862d6cc9ef23f65399449a0548fc1e321
mattmeyerink <meyerink@umich.edu>
2024-09-15 19:23:32 -0700

    add day of week and today markers to highlight view

--- src/planit/month/display.rs
+++ src/planit/month/display.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, Datelike, Local};
+use chrono::{DateTime, Datelike, Local, TimeZone};
 
 use super::data::MonthNote;
 
@@ -40,10 +40,18 @@ pub fn display_month_highlights(current_date: DateTime<Local>, month_highlights:
     println!("---------------------------------------------");
 
     // TODO -> add abbreviation for the day of the week next to each listed day.
+    // Also should add an astrisk if the day is today.
 
     for day_of_month_zeroed in 0..month_highlights.len() {
         let month_highlight = month_highlights.get(day_of_month_zeroed).unwrap();
-        let formatted_day_string = (day_of_month_zeroed + 1).to_string();
+
+        let day_object = Local.with_ymd_and_hms(current_date.year(), current_date.month(), (day_of_month_zeroed + 1).try_into().unwrap(), 0, 0, 0).unwrap();
+        let mut formatted_day_string = format!("{} {}", day_object.weekday(), &(day_of_month_zeroed + 1).to_string());
+
+        let todays_date = chrono::Local::now();
+        if format!("{}", todays_date.format("%Y-%m-%d")) == format!("{}", day_object.format("%Y-%m-%d")) {
+            formatted_day_string = format!("* {}", formatted_day_string);
+        }
 
         if month_highlight.len() > 0 {
             println!("{} - {}", formatted_day_string, month_highlight);
