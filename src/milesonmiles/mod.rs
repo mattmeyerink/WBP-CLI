be283edab8c545412e4ac7186b64156f0694e514
mattmeyerink <meyerink@umich.edu>
2024-09-29 18:20:06 -0700

    add miles on miles directory structure logic

--- src/milesonmiles/mod.rs
+++ src/milesonmiles/mod.rs
@@ -1,7 +1,6 @@
-use std::{fs, io::{self, Write}};
+use std::io::{self, Write};
 
 use chrono::Duration;
-use dirs::home_dir;
 
 mod utils;
 mod actions;
@@ -17,11 +16,7 @@ pub fn milesonmiles() {
     let mut current_date = utils::get_current_week_monday();
 
     loop {
-        // Make the planit data folder if it doesn't already exist
-        let wbp_plan_it_data_path = home_dir().unwrap().join("Documents").join("wbp-data").join("miles-on-miles");
-        if !wbp_plan_it_data_path.exists() {
-            fs::create_dir(&wbp_plan_it_data_path).expect("Unable to make plan it dir");
-        }
+        utils::create_miles_on_miles_directory_structure(current_date);
 
         // This is where we are going to print out data about the runs for this week.
 
@@ -41,9 +36,9 @@ pub fn milesonmiles() {
         io::stdin().read_line(&mut action).expect("Unable to read action");
 
         if action.trim() == "1" {
-            println!("Oopsies. Haven't done this one yet");
+            println!("Oopsies. Haven't done this one yet. This will be the log a run action");
         } else if action.trim() == "2" {
-            println!("Oopsies. Haven't done this one yet");
+            println!("Oopsies. Haven't done this one yet. This will be the ");
         } else if action.trim() == "3" {
             current_date = current_date - Duration::days(7);
         } else if action.trim() == "4" {
--- src/milesonmiles/utils.rs
+++ src/milesonmiles/utils.rs
@@ -1,8 +1,11 @@
+use std::fs;
+
 use chrono::DateTime;
 use chrono::Datelike;
 use chrono::Duration;
 use chrono::Local;
 use chrono::Weekday;
+use dirs::home_dir;
 
 pub fn get_current_week_monday() -> DateTime<Local> {
     let mut current_date = chrono::Local::now();
@@ -17,6 +20,17 @@ pub fn get_current_week_monday() -> DateTime<Local> {
     return current_date;
 }
 
-pub fn create_miles_on_miles_directory_structure() {
-    
+pub fn create_miles_on_miles_directory_structure(current_date: DateTime<Local>) {
+    // Make the milesonmiles data folder if it doesn't already exist
+    let wbp_miles_on_miles_data_path = home_dir().unwrap().join("Documents").join("wbp-data").join("miles-on-miles");
+    if !wbp_miles_on_miles_data_path.exists() {
+        fs::create_dir(&wbp_miles_on_miles_data_path).expect("Unable to make plan it dir");
+    }
+
+    // Create the outer dir for the current year if it doesn't already exist
+    let current_year = current_date.year().to_string();
+    let current_wbp_miles_on_miles_year_data_path = home_dir().unwrap().join("Documents").join("wbp-data").join("miles-on-miles").join(current_year.clone());
+    if !current_wbp_miles_on_miles_year_data_path.exists() {
+        fs::create_dir(&current_wbp_miles_on_miles_year_data_path).expect("Unable to make current year outer dir");
+    }
 }
