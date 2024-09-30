53cbc5fd1ad0aca274725763a81b63cd94e3ebcb
mattmeyerink <meyerink@umich.edu>
2024-09-29 18:02:28 -0700

    create outline of the miles on miles app structure

--- src/milesonmiles/mod.rs
+++ src/milesonmiles/mod.rs
@@ -1,5 +1,59 @@
+use std::{fs, io::{self, Write}};
+
+use chrono::Duration;
+use dirs::home_dir;
+
+mod utils;
+mod actions;
+mod data;
+mod display;
+
 pub fn milesonmiles() {
     println!("Welcome to Miles on Miles!");
 
-    println!("Were working on getting this going. Check back later!");
+    println!("");
+    println!("");
+
+    let mut current_date = utils::get_current_week_monday();
+
+    loop {
+        // Make the planit data folder if it doesn't already exist
+        let wbp_plan_it_data_path = home_dir().unwrap().join("Documents").join("wbp-data").join("miles-on-miles");
+        if !wbp_plan_it_data_path.exists() {
+            fs::create_dir(&wbp_plan_it_data_path).expect("Unable to make plan it dir");
+        }
+
+        // This is where we are going to print out data about the runs for this week.
+
+        println!("Actions you can take.");
+        println!("[1]: Log a run.");
+        println!("[2]: View run details.");
+        println!("[3]: Next week.");
+        println!("[4]: Previous week.");
+        println!("[5]: Quit miles on miles.");
+
+        println!("");
+
+        print!("Which action do you want to take: ");
+        io::stdout().flush().expect("Darn toilet got stuck again");
+
+        let mut action = String::new();
+        io::stdin().read_line(&mut action).expect("Unable to read action");
+
+        if action.trim() == "1" {
+            println!("Oopsies. Haven't done this one yet");
+        } else if action.trim() == "2" {
+            println!("Oopsies. Haven't done this one yet");
+        } else if action.trim() == "3" {
+            current_date = current_date - Duration::days(7);
+        } else if action.trim() == "4" {
+            current_date = current_date + Duration::days(7);
+        } else if action.trim() == "5" {
+            break;
+        } else {
+            println!("\n");
+            println!("Boooo thats not a correct action");
+            println!("\n");
+        }
+    }
 }
---
+++ src/milesonmiles/utils.rs
@@ -0,0 +1,22 @@
+use chrono::DateTime;
+use chrono::Datelike;
+use chrono::Duration;
+use chrono::Local;
+use chrono::Weekday;
+
+pub fn get_current_week_monday() -> DateTime<Local> {
+    let mut current_date = chrono::Local::now();
+    loop {
+        if current_date.weekday() == Weekday::Mon {
+            break;
+        } else {
+            current_date = current_date - Duration::days(1);
+        }
+    }
+
+    return current_date;
+}
+
+pub fn create_miles_on_miles_directory_structure() {
+    
+}
