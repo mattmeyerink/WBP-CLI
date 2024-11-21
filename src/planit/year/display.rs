a6d2129e49f3becb9583b610efd078939bd0f663
mattmeyerink <meyerink@umich.edu>
2024-11-21 16:54:23 -0700

    add better display order to list year results

--- src/planit/year/display.rs
+++ src/planit/year/display.rs
@@ -28,23 +28,57 @@ fn display_year_month_notes(month_notes: &Vec<YearNote>, display_id: bool, month
     println!("{}", month_names.get(month_to_display).unwrap());
     println!("--------------");
 
-    // TODO -> Sort the notes by type and by completion status
+    let event_symbol = "*";
+    let finished_task_symbol = "x";
+    let unfinished_task_symbol = "o";
+    let note_symbol = "-";
+
+    let mut events: Vec<&YearNote> = vec![];
+    let mut finished_tasks: Vec<&YearNote> = vec![];
+    let mut unfinished_tasks: Vec<&YearNote> = vec![]; 
+    let mut notes: Vec<&YearNote> = vec![];
+
     for month_note in month_notes {
-        let note_symbol;
         if month_note.note_type == "task" && month_note.is_complete == "true" {
-            note_symbol = "x";
+            finished_tasks.push(month_note)
         } else if month_note.note_type == "task" && month_note.is_complete == "false" {
-            note_symbol = "o";
+            unfinished_tasks.push(month_note)
         } else if month_note.note_type == "event" {
-            note_symbol = "*";
+            events.push(month_note)
         } else {
-            note_symbol = "-";
-        }
-        if display_id {
-            println!("{} {} ({})", note_symbol, month_note.note, month_note.note_id);
-        } else {
-            println!("{} {}", note_symbol, month_note.note);
+            notes.push(month_note)
         }
     }
+
+    let tasks_present = finished_tasks.len() > 0 || unfinished_tasks.len() > 0;
+    let print_space_after_events = events.len() > 0 && (tasks_present || notes.len() > 0);
+    let print_space_after_tasks = tasks_present && notes.len() > 0;
+
+    display_year_month_note_section(&event_symbol, display_id, events);
+
+    // Give some extra visual space if we have some after some events tasks incoming
+    if print_space_after_events {
+        println!("");
+    }
+
+    display_year_month_note_section(&finished_task_symbol, display_id, finished_tasks);
+    display_year_month_note_section(&unfinished_task_symbol, display_id, unfinished_tasks);
+
+    // Give some extra visual space if we have some notes incoming
+    if print_space_after_tasks {
+        println!("");
+    }
+
+    display_year_month_note_section(&note_symbol, display_id, notes);
     println!("");
+}
+
+fn display_year_month_note_section(note_symbol: &str, display_id: bool, section_notes: Vec<&YearNote>) {
+    for section_note in section_notes {
+        if display_id {
+            println!("{} {} ({})", note_symbol, section_note.note, section_note.note_id);
+        } else {
+            println!("{} {}", note_symbol, section_note.note);
+        }
+    }
 }
